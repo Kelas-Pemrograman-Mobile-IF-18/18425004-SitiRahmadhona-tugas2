f19ef846bc479525784174e467be015405a5f0f4
destrisilviraa <destri.18425003@student.ubl.ac.id>
2021-03-31 01:45:11 +0700

    tugas kedua saya

--- app/src/main/java/com/dhona/project1/MainActivity.java
+++ app/src/main/java/com/dhona/project1/MainActivity.java
@@ -3,18 +3,43 @@ package com.dhona.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtNama;
+    TextView txtTampil;
+    EditText edtNpm, edtNama, edtemail, edtNoHp, edtAlamat;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNama = (TextView) findViewById(R.id.txtNama);
-        txtNama.setText("Siti Rahmadhona");
+        txtTampil = (TextView) findViewById(R.id.txtNpmDanNama);
+        edtNpm = (EditText) findViewById(R.id.edtNpm);
+        edtNama = (EditText) findViewById(R.id.edtNama);
+        edtemail = (EditText) findViewById(R.id.edtemail);
+        edtNoHp = (EditText) findViewById(R.id.edtNoHp);
+        edtAlamat = (EditText) findViewById(R.id.edtAlamat);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNpm = edtNpm.getText().toString();
+                String strNama = edtNama.getText().toString();
+                String stremail = edtemail.getText().toString();
+                String strNoHp = edtNoHp.getText().toString();
+                String strAlamat = edtAlamat.getText().toString();
+
+                txtTampil.setText(strNpm + "\n" + strNama + "\n" + stremail + "\n" + strNoHp + "\n" + strAlamat);
+
+            }
+        });
+
     }
 }
